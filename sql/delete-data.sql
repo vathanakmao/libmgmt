061b5b6d8a83357a339c174b25a5aacd8e9d70c5
Vathanak Mao @Work <vathanakmao.atwork@gmail.com>
2016-12-08 04:49:02 +0700

    Add sql files to insert and delete data

---
+++ sql/delete-data.sql
@@ -0,0 +1,3 @@
+delete from book;
+delete from librarian;
+delete from member;
---
+++ sql/insert-data.sql
@@ -0,0 +1,11 @@
+/** book */
+insert into book values (1, '111', 'Java Programming', 'Vathanak Mao', 2017);
+insert into book values (2, '222', '.Net Programming', 'Vathanak Mao', 2018);
+insert into book values (3, '333', 'Data Structure', 'Sok San', 2019);
+
+/** librarian */
+insert into librarian values (1, 'Jonh', 'Smith', 'M', now(), now());
+insert into librarian values (2, 'Britney', 'Spear', 'F', now(), now());
+
+/** member */
+insert into member values ('g5938444', 'Vathanak', 'Mao', 'M', now(), now());
